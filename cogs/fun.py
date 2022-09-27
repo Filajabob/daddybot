98590178fdee324ac4ada1b7766b1471e87d3382
Liam <98435043+Filajabob@users.noreply.github.com>
2022-09-26 21:15:21 -0400

    Added XP setting and other small things

--- cogs/fun.py
+++ cogs/fun.py
@@ -15,6 +15,7 @@ class FunCog(commands.Cog):
     quote = SlashCommandGroup("quote", "Get a quote")
 
     @quote.command(name="breaking-bad", description="Get a quote from the TV show Breaking Bad")
+    @commands.cooldown(1, 7, commands.BucketType.user)
     async def breaking_bad(self, ctx):
         url = "https://www.breakingbadapi.com/api/quote/random"
         r = json.loads(requests.get(url).content.decode())
@@ -22,6 +23,7 @@ class FunCog(commands.Cog):
         await ctx.respond(f"{r[0]['quote']}\n-{r[0]['author']}")
 
     @quote.command(name="the-office", description="Get a quote from the TV show The Office (US)")
+    @commands.cooldown(1, 7, commands.BucketType.user)
     async def the_office(self, ctx):
         with open("assets/bot/quotes/the-office/the-office-quotes.json", 'r') as f:
             data = json.load(f)
