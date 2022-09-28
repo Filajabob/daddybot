365b5da0c7cf10c318409d85aa764961ff967f55
Liam <98435043+Filajabob@users.noreply.github.com>
2022-09-28 18:55:07 -0400

    Added dev_bot directory for developing

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .constants import Constants
 from .xp import add, subtract, set_amount, get_amount
+from .is_dev import is_dev
---
+++ utils/is_dev.py
@@ -0,0 +1,2 @@
+def is_dev(client):
+    return not client.user.id == 1021522540226691183
